a2883d4878dc41a457a28b85cec1e5037795a58b
Tim Lu <tim@tims-mbp.home>
2022-02-18 12:37:59 +0000

    Add Messages

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,7 +4,22 @@ class RoomsController < ApplicationController
   def index
     @room = Room.new
     @rooms = Room.public_rooms
+
     @users = User.all_except(current_user)
+    render 'index'
+  end
+
+  def show
+    @single_room = Room.find(params[:id])
+
+    @room = Room.new
+    @rooms = Room.public_rooms
+
+    @message = Message.new
+    @messages = @single_room.messages.order(created_at: :asc)
+
+    @users = User.all_except(current_user)
+    render 'index'
   end
 
   def create
